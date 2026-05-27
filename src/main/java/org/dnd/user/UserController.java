35db8c26ad953cc6632cccb55f79a4cc29418c65
Szii <bruneksemerek@seznam.cz>
2026-05-27 21:02:32 +0200

    fix: email change

--- src/main/java/org/dnd/user/UserController.java
+++ src/main/java/org/dnd/user/UserController.java
@@ -60,6 +60,19 @@ public class UserController implements UsersApi {
   }
 
   @Override
+  public ResponseEntity<Void> changeUnverifiedEmail(UserRegisterRequest userAuthDTO) {
+    userService.changeUnverifiedEmail(userAuthDTO.getName(), userAuthDTO.getPassword(), userAuthDTO.getEmail());
+    return ResponseEntity.ok().build();
+  }
+
+  @Override
+  public ResponseEntity<Void> changeVerifiedEmail(UserRegisterRequest userAuthDTO) {
+    userService.changeVerifiedEmail(userAuthDTO.getName(), userAuthDTO.getPassword(), userAuthDTO.getEmail());
+    return ResponseEntity.ok().build();
+  }
+
+
+  @Override
   @RateLimiting(
           name = "default-api",
           cacheKey = "@rateLimitKeyResolver.currentUserKey()",
