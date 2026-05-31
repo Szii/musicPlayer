a2c7391c613b512821c2c188531569142f1a9bc4
Szii <79159773+Szii@users.noreply.github.com>
2026-05-31 23:56:03 +0200

    fix: add youtube client list (#45)

--- src/main/java/org/dnd/configuration/AudioPlayerConfiguration.java
+++ src/main/java/org/dnd/configuration/AudioPlayerConfiguration.java
@@ -4,18 +4,30 @@ import com.sedmelluq.discord.lavaplayer.format.StandardAudioDataFormats;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import dev.lavalink.youtube.YoutubeAudioSourceManager;
+import dev.lavalink.youtube.clients.AndroidVr;
+import dev.lavalink.youtube.clients.Ios;
+import dev.lavalink.youtube.clients.MWeb;
+import dev.lavalink.youtube.clients.Music;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AudioPlayerConfiguration {
-    @Bean
-    public AudioPlayerManager audioPlayerManager() {
-        AudioPlayerManager mgr = new DefaultAudioPlayerManager();
-        mgr.getConfiguration().setOutputFormat(StandardAudioDataFormats.COMMON_PCM_S16_LE);
 
-        mgr.registerSourceManager(new YoutubeAudioSourceManager());
+  @Bean
+  public AudioPlayerManager audioPlayerManager() {
+    AudioPlayerManager mgr = new DefaultAudioPlayerManager();
+    mgr.getConfiguration().setOutputFormat(StandardAudioDataFormats.COMMON_PCM_S16_LE);
 
-        return mgr;
-    }
+    YoutubeAudioSourceManager youtube = new YoutubeAudioSourceManager(
+            true,
+            new Music(),
+            new AndroidVr(),
+            new Ios(),
+            new MWeb());
+
+    mgr.registerSourceManager(youtube);
+
+    return mgr;
+  }
 }
