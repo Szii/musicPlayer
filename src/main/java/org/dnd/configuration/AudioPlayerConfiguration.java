24fffa0b7971c0f899ea9a8ada86eef1dd18baaf
Szii <bruneksemerek@seznam.cz>
2026-06-01 00:41:00 +0200

    fix: oauth compatible client

--- src/main/java/org/dnd/configuration/AudioPlayerConfiguration.java
+++ src/main/java/org/dnd/configuration/AudioPlayerConfiguration.java
@@ -4,10 +4,7 @@ import com.sedmelluq.discord.lavaplayer.format.StandardAudioDataFormats;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import dev.lavalink.youtube.YoutubeAudioSourceManager;
-import dev.lavalink.youtube.clients.AndroidVr;
-import dev.lavalink.youtube.clients.Ios;
-import dev.lavalink.youtube.clients.MWeb;
-import dev.lavalink.youtube.clients.Music;
+import dev.lavalink.youtube.clients.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,7 +28,8 @@ public class AudioPlayerConfiguration {
             new Music(),
             new AndroidVr(),
             new Ios(),
-            new MWeb()
+            new MWeb(),
+            new Tv()
     );
 
     if (youtubeOauthRefreshToken != null && !youtubeOauthRefreshToken.isBlank()) {
